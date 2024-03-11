af3bcc187e41b7fd88bf3659e438dd4bb6e23c23
xsanket <xsanketkamble232@gmail.com>
2024-03-11 19:50:06 +0530

    deployment config

--- back-end/server.js
+++ back-end/server.js
@@ -25,9 +25,9 @@ const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/front-end/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "front-end", "build", "index.html"));
   });
 }
 
